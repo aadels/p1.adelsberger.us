7a7cbbd9adc6f35e6464e767b28adb70c2325f3e
ann adelsberger <adelsbergerann@gmail.com>
2013-09-15 19:59:57 -0400

    added first draft of bio text

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <html> 
 <body> 
 <?php 
-echo "<h4>Welcome to P1!</h4>"; 
-echo "testing workflow"; 
-echo "<br>";
-echo "re-testing workflow"; 
+echo "<h4>Ann Adelsberger/h4>"; 
+echo "I live in Somerville, MA and work full time at MIT Libraries in the Communications 
+Department. I am a degree candidate in the ALM Digital Media and Instructional Design program. 
+This will be my last semester of coursework before statring my thesis project. I'm enrolled in
+DGMD E-30 (Video Field Production) as well ast CSCI E-15. I took CSCI E-52 last fall and learned 
+PHP for my final project. Learning programming in CSCI E-52 was like drinking from a firehose, 
+so I am really looking forward to learning more PHP at what I'm sure will be a challenging but 
+more reasonable pace. I have some experience with GIS and would also like to learn Python in 
+order to take on some more complex projects using ArcGIS. I am also a huge fan of PhotoJoJo!
+I am using Sublime text as my text editor."; 
+
 ?> 
 </body> 
 </html>
