10e1589a14631eebe406009dd03d8dabf96003c1
ann adelsberger <adelsbergerann@gmail.com>
2013-09-16 07:10:59 -0400

    added link to style.css in head

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"
-      lang="en">> 
+      lang="en"> 
     <head>
-   	<title>Ann Adelsberger - a short bio</title>
+   		<title>Ann Adelsberger - a short bio</title>
+   		<link rel="stylesheet" type="text/css" href="style.css" /> 
+    </head>
 	<body> 
 		<?php 
 			echo 	"<h4>Ann Adelsberger</h4>"; 
@@ -12,13 +14,13 @@
 				<a href= "http://libraries.mit.edu">MIT Libraries</a> 
 		<?php
 			echo	"in the Communications Department. I am a degree candidate in the ALM Digital Media and Instructional Design program. 
-					This will be my last semester of coursework before statring my thesis project. I'm enrolled in
-					DGMD E-30 (Video Field Production) as well as CSCI E-15. I took CSCI E-52 last fall and learned 
-					PHP for my final project. Learning programming in CSCI E-52 was like drinking from a firehose, 
-					so I am really looking forward to learning more PHP at what I'm sure will be a challenging but 
-					more reasonable pace. I have some experience with GIS and would also like to learn Python in 
-					order to take on some more complex projects using ArcGIS. I am also a huge fan of PhotoJoJo, 
-					especially the"; ?> 
+					This will be my last semester of coursework and in January I will start work on my my thesis project. I'm enrolled in
+					DGMD E-30 (Video Field Production) as well as CSCI E-15. My programming skills are fairly modest atthis point. I took 
+					CSCI E-52 last fall and learned basic PHP for my final project. Learning programming in CSCI E-52 was like drinking 
+					from a firehose, so I am really looking forward to learning more PHP at what I'm sure will be a challenging but 
+					more reasonable pace. I have some experience with GIS and would also like to learn Python in order to take on some more 
+					complex projects using ArcGIS. I am also a huge fan of PhotoJoJo, especially the"; 
+		?> 
 				<a href= "http://content.photojojo.com/"> Inspriation & Projects </a> 
 		<?php 
 			echo 	"section! I am using Sublime Text as my text editor."; 
