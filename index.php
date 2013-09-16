b0cffb4a1fc5e519e5007e38423a52bf5839a7b4
ann adelsberger <adelsbergerann@gmail.com>
2013-09-15 20:35:18 -0400

    edited text and added links in bio

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <html> 
 <body> 
+	<?php 
+		echo 	"<h4>Ann Adelsberger</h4>"; 
+		echo 	"I live in Somerville, MA and work full time at the"
+	?>  
+			<a href= "http://libraries.mit.edu">MIT Libraries</a> 
+	<?php
+		echo	"in the Communications Department. I am a degree candidate in the ALM Digital Media and Instructional Design program. 
+				This will be my last semester of coursework before statring my thesis project. I'm enrolled in
+				DGMD E-30 (Video Field Production) as well as CSCI E-15. I took CSCI E-52 last fall and learned 
+				PHP for my final project. Learning programming in CSCI E-52 was like drinking from a firehose, 
+				so I am really looking forward to learning more PHP at what I'm sure will be a challenging but 
+				more reasonable pace. I have some experience with GIS and would also like to learn Python in 
+				order to take on some more complex projects using ArcGIS. I am also a huge fan of PhotoJoJo, 
+				especially the"; ?> 
+			<a href= "http://content.photojojo.com/"> Inspriation & projects </a> 
 <?php 
-echo "<h4>Ann Adelsberger</h4>"; 
-echo "I live in Somerville, MA and work full time at MIT Libraries in the Communications 
-Department. I am a degree candidate in the ALM Digital Media and Instructional Design program. 
-This will be my last semester of coursework before statring my thesis project. I'm enrolled in
-DGMD E-30 (Video Field Production) as well as CSCI E-15. I took CSCI E-52 last fall and learned 
-PHP for my final project. Learning programming in CSCI E-52 was like drinking from a firehose, 
-so I am really looking forward to learning more PHP at what I'm sure will be a challenging but 
-more reasonable pace. I have some experience with GIS and would also like to learn Python in 
-order to take on some more complex projects using ArcGIS. I am also a huge fan of PhotoJoJo, 
-especially the"; ?> <a href= "http://content.photojojo.com/"> Inspriation & projects </a> 
-<?php 
-echo "section! I am using Sublime Text as my text editor."; 
+		echo 	"section! I am using Sublime Text as my text editor."; 
 
 ?> 
 </body> 
